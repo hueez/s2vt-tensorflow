0d003108e318e688ec3a9821ea567f46c74bcfc5
hueez <jaehui@europa.snu.ac.kr>
2017-08-18 12:13:23 +0900

    remove image resize option in ffmpeg command

--- extract_jpgs_from_video_lsmdc2017.py
+++ extract_jpgs_from_video_lsmdc2017.py
@@ -1,7 +1,8 @@
 import os
 
 root_dir = "/hdd4/lsmdc2017/"
-movie_list = [name for name in os.listdir(root_dir) if (os.path.isdir(os.path.join(root_dir, name)) and name[0].isdigit())]
+#movie_list = [name for name in os.listdir(root_dir) if (os.path.isdir(os.path.join(root_dir, name)) and name[0].isdigit())]
+movie_list = ['0002_As_Good_As_It_Gets', '0005_Chinatown', '0007_DIE_NACHT_DES_JAEGERS', '0008_Fargo']
 
 num_movies = len(movie_list)
 print "Total %d movies\n" % num_movies
@@ -23,5 +24,6 @@ for movie in movie_list:
     for clip in clip_list:
         clip_file = os.path.join(movie_sub_dir, clip)
         jpg_file = os.path.join(jpg_sub_dir, clip)
-        cmd = [ 'ffmpeg -i ' + clip_file + ' -r 24 -an -threads 0 -f image2 ' + jpg_file[:jpg_file.rfind('.')] + '_fn_%03d.jpg' ]
+        os.makedirs(os.path.join(jpg_sub_dir, jpg_file[:jpg_file.rfind('.')]))
+        cmd = ['ffmpeg -i ' + clip_file + ' -r 24 -an -threads 0 -f image2 ' + jpg_file[:jpg_file.rfind('.')] + "/" + jpg_file[jpg_file.rfind('/')+1:jpg_file.rfind('.')] + '_fn_%04d.jpg']
         os.system(cmd[0])
